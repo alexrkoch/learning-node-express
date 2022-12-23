5e6df8ad0f9d329950f302d84c938789c05735d3
Alex <alexrkoch@gmail.com>
2022-12-23 13:09:13 -0500

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
-const express = require('express');
+import { Request, Response, NextFunction } from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set up home route
+app.get("/", (req: Request, res: Response) => {
+  res.send("This is the homepage");
+});
+
+// Set up second page
+app.get("/second", (req: Request, res: Response) => {
+  res.send("This is the second page");
+});
 
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
