2cebc8485078341377d9ad95475f2f32e965f5d1
Alex <alexrkoch@gmail.com>
2022-12-23 12:47:25 -0500

    initial commit, ts and express setup

---
+++ src/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var port = 4000;
+app.listen(port, function () {
+    console.log("Success! Your application is running on port ".concat(port, "."));
+});
+//# sourceMappingURL=server.js.map
